2597eaeff0aa8ed54681ec774817beda9ddf769c
Camila Amaro <camilaamarov@yahoo.es>
2024-10-30 18:42:32 -0500

    feat: Configured Angular project with Tailwind CSS and added PortfolioDetails component

--- bytebank/src/app/app.routes.ts
+++ bytebank/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PortfolioDetailsComponent } from './components/portfolio-details/portfolio-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'home', component: PortfolioDetailsComponent}
+];
