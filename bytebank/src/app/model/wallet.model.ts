a26c5f25904f75d09001d72a67f5a7c19341e7b6
Camila Amaro <camilaamarov@yahoo.es>
2024-11-13 13:54:41 -0500

    feat: Add wallet model and service

--- bytebank/src/app/model/wallet.model.ts
+++ bytebank/src/app/model/wallet.model.ts
@@ -1,2 +1,33 @@
 export class Wallet {
+  id: any;
+  name: any;
+  interestRate: any;
+  interestType: any;
+  interestPeriod: any;
+  capitalizationPeriod: any;
+  activationFee: any;
+  port: any;
+  retentionPercentage: any;
+  administrativeExpenses: any;
+  mortgageInsurance: any;
+  riskInsurance: any;
+  operationTerm: any;
+  discountDate: any;
+
+  constructor(id: any, name: any, interestRate: any, interestType: any, interestPeriod: any, capitalizationPeriod: any, activationFee: any, port: any, retentionPercentage: any, administrativeExpenses: any, mortgageInsurance: any, riskInsurance: any, operationTerm: any, discountDate: any) {
+    this.id = id;
+    this.name = name;
+    this.interestRate = interestRate;
+    this.interestType = interestType;
+    this.interestPeriod = interestPeriod;
+    this.capitalizationPeriod = capitalizationPeriod;
+    this.activationFee = activationFee;
+    this.port = port;
+    this.retentionPercentage = retentionPercentage;
+    this.administrativeExpenses = administrativeExpenses;
+    this.mortgageInsurance = mortgageInsurance;
+    this.riskInsurance = riskInsurance;
+    this.operationTerm = operationTerm;
+    this.discountDate = discountDate;
+  }
 }
--- bytebank/src/app/services/wallet.service.ts
+++ bytebank/src/app/services/wallet.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {environment} from '../../environment/environment';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WalletService {
+  baseUrl:string = environment.baseUrl;
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  createWallet(wallet: any) {
+    return this.http.post(this.baseUrl + '/wallet', wallet);
+  }
 }
