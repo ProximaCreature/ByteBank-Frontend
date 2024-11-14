04ba272dc2d1c27fdbbe0f85064b1be966a6a904
steve <stev8855@gmail.com>
2024-11-14 10:56:22 -0500

    feat: implement registration form with validation and password visibility toggle, first version

--- bytebank/src/app/views/pages/register/register.component.ts
+++ bytebank/src/app/views/pages/register/register.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  private _snackBar = inject(MatSnackBar);
+  private _formBuilder = inject(FormBuilder);
+  private _router = inject(Router);
 
+  form: FormGroup = this._formBuilder.group({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', Validators.required),
+    confirmPassword: new FormControl('', Validators.required),
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    acceptTerms: new FormControl(false, Validators.requiredTrue)
+  });
+
+  passwordVisible: boolean = false;
+  confirmPasswordVisible: boolean = false;
+
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  toggleConfirmPasswordVisibility(): void {
+    this.confirmPasswordVisible = !this.confirmPasswordVisible;
+  }
+
+  register(): void {
+    if (this.form.invalid) {
+      this._snackBar.open('Por favor, completa todos los campos correctamente', 'Cerrar', {
+        duration: 3000,
+        horizontalPosition: 'right',
+        verticalPosition: 'top',
+      });
+      return;
+    }
+
+    console.log(this.form.value);
+
+    this._router.navigate(['/login']);
+  }
 }
