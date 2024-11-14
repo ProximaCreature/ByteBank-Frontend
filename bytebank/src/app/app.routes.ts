69a9eb0f57569d42c95ce0f87e9000d7496fd13c
steve <stev8855@gmail.com>
2024-11-14 08:44:36 -0500

    feat: add AuthRedirectGuard to login and register routes for authentication management

--- bytebank/src/app/app.routes.ts
+++ bytebank/src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { AuthRedirectGuard } from '@controllers/auth-redirect.guard';
 import { isAthenticatedGuard } from '@controllers/is-athenticated.guard';
 
 export const routes: Routes = [
@@ -13,10 +14,12 @@ export const routes: Routes = [
     children: [
       {
         path: 'login',
+        canActivate: [AuthRedirectGuard],
         loadComponent: () => import('@views/pages/login/login.component').then(m => m.LoginComponent),
       },
       {
         path: 'register',
+        canActivate: [AuthRedirectGuard],
         loadComponent: () => import('@views/pages/register/register.component').then(m => m.RegisterComponent),
       },
     ]
