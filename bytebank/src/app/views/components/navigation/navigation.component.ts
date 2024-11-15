ca1076227ee189908c4abc5123c982eb0bfdbd19
steve <stev8855@gmail.com>
2024-11-15 16:12:44 -0500

    feat: refactor navigation component styles and remove unused icon components

--- bytebank/src/app/views/components/navigation/navigation.component.ts
+++ bytebank/src/app/views/components/navigation/navigation.component.ts
@@ -4,10 +4,6 @@ import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 import { LogoComponent } from '../shared/logo/logo.component';
 import { UserIconComponent } from '../shared/user-icon/user-icon.component';
-import { AddIconComponent } from '../shared/add-icon/add-icon.component';
-import { PaymentIconComponent } from '../shared/payment-icon/payment-icon.component';
-import { WalletIconComponent } from '../shared/wallet-icon/wallet-icon.component';
-
 @Component({
   selector: 'app-navigation',
   standalone: true,
@@ -17,9 +13,6 @@ import { WalletIconComponent } from '../shared/wallet-icon/wallet-icon.component
     CommonModule,
     LogoComponent,
     UserIconComponent,
-    AddIconComponent,
-    PaymentIconComponent,
-    WalletIconComponent
   ],
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
