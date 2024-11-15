3fc93d48134d21b9a327dfe1002d34ab798d1535
steve <stev8855@gmail.com>
2024-11-15 12:27:01 -0500

    feat: update logo component with responsive dimensions and improved SVG attributes

--- bytebank/src/app/views/components/shared/logo/logo.component.ts
+++ bytebank/src/app/views/components/shared/logo/logo.component.ts
@@ -2,12 +2,12 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-logo',
+  standalone: true,
   templateUrl: './logo.component.html',
-  styleUrls: ['./logo.component.css'],
-  standalone: true
+  styleUrls: ['./logo.component.css']
 })
 export class LogoComponent {
-  @Input() width: string = '100px';
-  @Input() height: string = '100px';
+  @Input() width: string = '40px';
+  @Input() height: string = '40px';
   @Input() fillColor: string = '#02273A';
 }
