1951c98f96fe9dca945ad966c038e25967f328e3
steve <stev8855@gmail.com>
2024-11-15 16:33:02 -0500

    feat: add profile menu with logout functionality and improve navigation component interactions

--- bytebank/src/app/views/components/navigation/navigation.component.ts
+++ bytebank/src/app/views/components/navigation/navigation.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'; // Importa Router
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
+
+import { AuthService } from '@services/auth.service';
 import { LogoComponent } from '../shared/logo/logo.component';
 import { UserIconComponent } from '../shared/user-icon/user-icon.component';
+
 @Component({
   selector: 'app-navigation',
   standalone: true,
@@ -19,8 +23,26 @@ import { UserIconComponent } from '../shared/user-icon/user-icon.component';
 })
 export class NavigationComponent {
   menuOpen = false;
+  profileOpen = false;
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+    if (this.profileOpen) {
+      this.profileOpen = false;
+    }
+  }
+
+  toggleProfile() {
+    this.profileOpen = !this.profileOpen;
+    if (this.menuOpen) {
+      this.menuOpen = false;
+    }
+  }
+
+  logOut() {
+    this.authService.logOut();
+    this.router.navigate(['/login']);
   }
 }
