e606b814737d6aaa7246dd057473059aeceeca0c
steve <stev8855@gmail.com>
2024-11-17 11:56:38 -0500

    feat: Implement createBill method in BillController and BillService

--- bytebank/src/app/services/bill.service.ts
+++ bytebank/src/app/services/bill.service.ts
@@ -1,19 +1,30 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {environment} from '../../environment/environment';
-import {AuthService} from '@services/auth.service';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environment/environment';
+import { AuthService } from '@services/auth.service';
+import { Observable } from 'rxjs';
+import { Bill } from '@models/bill.model';
+import { CreateBillCommand } from '@models/create-bill-command.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BillService {
-  baseUrl:string = environment.baseUrl;
+  baseUrl: string = environment.baseUrl;
+
   constructor(private http: HttpClient, private authService: AuthService) { }
 
-  getByWalletId(walletId: number) {
+  getByWalletId(walletId: number): Observable<Bill[]> {
     const headers = {
       Authorization: `Bearer ${this.authService.token}`
-    }
-    return this.http.get(`${this.baseUrl}/wallets/${walletId}`, { headers });
+    };
+    return this.http.get<Bill[]>(`${this.baseUrl}/bills?walletId=${walletId}`, { headers });
+  }
+
+  createBill(command: CreateBillCommand) {
+    const headers = {
+      Authorization: `Bearer ${this.authService.token}`
+    };
+    return this.http.post(`${this.baseUrl}/bills`, command, { headers });
   }
 }
